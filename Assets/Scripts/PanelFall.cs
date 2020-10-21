99df270eca334abf0ad4b2f8acbc62bcbc02afd8
meinu-chan <62944053+meinu-chan@users.noreply.github.com>
2020-10-21 13:40:03 +0300

    Added Falling Panels

---
+++ Assets/Scripts/PanelFall.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelFall : MonoBehaviour
+{
+    Rigidbody rigidBody;
+    public float sec;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        StartCoroutine(CorouitineSample());
+    }
+
+    public IEnumerator CorouitineSample(){
+        yield return new WaitForSeconds(sec);
+        FallDown();
+    }
+
+    private void FallDown(){
+        rigidBody.isKinematic = false;
+    }
+}
