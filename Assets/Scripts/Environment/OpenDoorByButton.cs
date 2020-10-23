404d64310399f184ff3e3abc855c0c44415407ef
meinu-chan <62944053+meinu-chan@users.noreply.github.com>
2020-10-23 19:35:09 +0300

    Some push 0_o

---
+++ Assets/Scripts/Environment/OpenDoorByButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoorByButton : MonoBehaviour
+{
+    public GameObject obj;
+
+    private void OnTriggerEnter(Collider other) {
+        
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void upDoor(){}
+}
